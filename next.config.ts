fe89a9acc17c3e8976b2c08b6486c36ab508fc93
Etienne van Vrijaldenhoven <etienne__@hotmail.com>
2026-09-24 22:52:46 +0200

    herstellende werkzaamheden

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  allowedDevOrigins: ['192.168.178.234'], // vervang door jouw eigen IPv4-adres
+}
 
 export default nextConfig;
